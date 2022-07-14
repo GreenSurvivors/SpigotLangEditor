5fef0fbed92c9894ba684d014c3b4efa59ca2c6e
Schpammer <schpammer@greensurvivors.de>
2022-07-14 13:46:13 +0200

    Implementing editor and preview

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,42 @@ function copyNormalizeCSS(cb) {
     cb();
 }
 
+function copyJsYaml(cb) {
+    src('node_modules/js-yaml/dist/js-yaml.min.js')
+    .pipe(dest('dist/js/'));
+    cb();
+}
+
+function copyCodemirror(cb) {
+    src('node_modules/codemirror/lib/*.js')
+    .pipe(dest('dist/js/codemirror/'));
+
+    src('node_modules/codemirror/lib/*.css')
+    .pipe(dest('dist/css/codemirror/'));
+
+    src('node_modules/codemirror/mode/**/*.js')
+    .pipe(dest('dist/js/codemirror/mode/yaml/'));
+
+    src('node_modules/codemirror/theme/cobalt.css')
+    .pipe(dest('dist/css/codemirror/theme/'));
+
+    src('node_modules/codemirror/addon/search/*.js')
+    .pipe(dest('dist/js/codemirror/addon/search/'));
+
+    src('node_modules/codemirror/addon/search/*.css')
+    .pipe(dest('dist/css/codemirror/addon/search/'));
+
+    src('node_modules/codemirror/addon/dialog/*.js')
+    .pipe(dest('dist/js/codemirror/addon/dialog/'));
+
+    src('node_modules/codemirror/addon/dialog/*.css')
+    .pipe(dest('dist/css/codemirror/addon/dialog/'));
+
+    src('node_modules/codemirror/addon/scroll/*.js')
+    .pipe(dest('dist/js/codemirror/addon/scroll/'));
+    cb()
+}
+
 function copyHtml(cb) {
     src('src/*.html')
     .pipe(dest('dist/'));
@@ -30,7 +66,13 @@ function copyJavascript(cb) {
     cb()
 }
 
-exports.default =  parallel(copyHtml, copyCss, copyJavascript, copyMiligram, copyNormalizeCSS);
+function copyFont(cb) {
+    src('src/font/*.ttf')
+    .pipe(dest('dist/font'));
+    cb()
+}
+
+exports.default =  parallel(copyHtml, copyCss, copyJavascript, copyFont, copyMiligram, copyNormalizeCSS, copyJsYaml, copyCodemirror);
 exports.watch = function() {
     watch('src/*.html', copyHtml);
 }
--- src/js/sle.js
+++ src/js/sle.js
@@ -1,33 +1,75 @@
-configField = document.querySelector('#config-field');
-previewBtn = document.querySelector('#preview-btn');
-preview = document.querySelector('#preview');
-previewBtn.onclick = function() {
-    text = colorizeMinecraft(configField.value);
-    preview.innerHTML = text;
-} 
+import "./js-yaml.min.js";
+import "./codemirror/codemirror.js";
+import "./codemirror/mode/yaml/yaml.js"
+import "./codemirror/addon/search/search.js"
+import "./codemirror/addon/search/searchcursor.js"
+import "./codemirror/addon/search/jump-to-line.js"
+import "./codemirror/addon/search/matchesonscrollbar.js"
+import "./codemirror/addon/dialog/dialog.js"
+import "./codemirror/addon/scroll/annotatescrollbar.js"
 
+
+const editor = createCodemirrorEditor();
+editor.setSize('100%', '100%');
+
+editor.on("change", function (cm, changeObj) {
+    refreshPreview();
+});
+
+const showKeys = document.querySelector('#showKeys')
+showKeys.onclick = function() {
+    refreshPreview();
+};
+
+/**
+ * Creates the Editor
+ * @returns Codemirror-Object
+ */
+function createCodemirrorEditor() {
+    return CodeMirror.fromTextArea(document.querySelector('#config-field'), {
+        lineNumbers: true,
+        mode: 'text/x-yaml',
+        matchBrackets: true,
+        indentUnit: 2,
+        tabSize: 2,
+        indentWithTabs: false,
+        theme: 'cobalt',
+        extraKeys: {"Alt-F": "findPersistent"}
+
+    });
+}
+
+/**
+ * Refreshes the preview
+ */
+function refreshPreview() {
+    let preview = document.querySelector('#preview');
+    let showKeysCheckbox = document.querySelector('#showKeys');
+
+    let config = jsyaml.load(editor.getValue());
+
+    let map = flattenObjectToMap(config);
+
+    const showKeys = showKeysCheckbox.checked;
+
+    let output = "";
+
+    for (var key in map) {
+        if (showKeys === true) {
+            output += `<h5>${key}</h5>`;
+        }
+        let value = colorizeMinecraft(map[key]);
+        output += `<p class="minecraft-font">${value}</p>`
+    }
+    preview.innerHTML = output;   
+}
+
+/**
+ * Replaces Bukkit color codes with corresponding HTML elements
+ * @param {*} string 
+ * @returns 
+ */
 function colorizeMinecraft(string) {
-
-    // $string = preg_replace('/&([0-9a-f])/i', '<span class="mc-color mc-$1">', $string, -1, $count) . str_repeat("</span>", $count);
-    // $string = utf8_encode(preg_replace('/&([k-or])/i', '<span class="mc-$1">', $string, -1, $count) . str_repeat("</span>", $count));
-    // let isOpenSpan = false;
-
-    // string = string.replace(/&([0-9a-f])/ig, function(match, p1) {
-    //     if (isOpenSpan === true) {
-    //         return `</span><span class=\"minecraft mc-code-${p1}\">`;
-    //     }
-    //     isOpenSpan = true;
-    //     return `<span class=\"minecraft mc-code-${p1}\">`;
-    // });
-
-    // if (isOpenSpan === true) {
-    //     string += '</span>';
-    // }
-
-    // // for (let index = 0; index < count; index++) {
-    // //     string += "</span>";
-    // // }
-
     let count = 0
     string = string.replace(/&([0-9a-f])/ig, function(match, p1) {
         count++;
@@ -49,6 +91,33 @@ function colorizeMinecraft(string) {
     
 
     return string;
+};
+
+/**
+ * Takes an object and creates a map with keys equally to the objects structure, seperated with dots.
+ * @param {*} obj 
+ * @param {*} stack 
+ * @returns 
+ */
+function flattenObjectToMap(obj, stack = "") {
+    let flattened = [];
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+                let flattenedChildren = flattenObjectToMap(obj[property], stack + (stack.length > 0 ? '.' : '') + property);
+                flattened = {...flattened, ...flattenedChildren};
+            } else {
+                let key = stack + (stack.length > 0 ? '.' : '') + property;
+                let value = obj[property]
+                flattened[key] = value;
+            }
+        }
+    }
+    return flattened
 }
+
+
+
+
 
 
